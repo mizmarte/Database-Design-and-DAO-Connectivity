969057ec38ece8b6d338bec3c027e198051548d7
Marte Shepherd <blootaz@rocketmail.com>
2021-01-20 08:24:22 -0500

    deleted projects_pair db & connection, and recreated projects db and connection

--- module-2/06_Database_Connectivity_DAO/pair-exercise/java/database/create_projects_database.sql
+++ module-2/06_Database_Connectivity_DAO/pair-exercise/java/database/create_projects_database.sql
@@ -1,5 +1,5 @@
-SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'projects_pair';
+SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'projects';
 
-DROP DATABASE projects_pair;
+DROP DATABASE projects;
 
-CREATE DATABASE projects_pair;
+CREATE DATABASE projects;
