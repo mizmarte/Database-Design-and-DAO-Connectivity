9847bfecb2f37bd19144a34d229dc60b696e03cc
Justin Frey <firerunswyld@gmail.com>
2021-01-20 14:29:05 -0700

    progress

--- module-2/06_Database_Connectivity_DAO/pair-exercise/java/src/main/java/com/techelevator/projects/model/jdbc/JDBCProjectDAO.java
+++ module-2/06_Database_Connectivity_DAO/pair-exercise/java/src/main/java/com/techelevator/projects/model/jdbc/JDBCProjectDAO.java
@@ -1,35 +1,82 @@
 package com.techelevator.projects.model.jdbc;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import com.techelevator.projects.model.Project;
 import com.techelevator.projects.model.interfaces.ProjectDAO;
 
-public class JDBCProjectDAO implements ProjectDAO {
+public class JDBCProjectDAO implements ProjectDAO
+{
 
 	private JdbcTemplate jdbcTemplate;
 
-	public JDBCProjectDAO(DataSource dataSource) {
+	public JDBCProjectDAO(DataSource dataSource)
+	{
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
-	
-	@Override
-	public List<Project> getAllActiveProjects() {
-		return new ArrayList<>();
-	}
 
 	@Override
-	public void removeEmployeeFromProject(Long projectId, Long employeeId) {
+	public List<Project> getAllActiveProjects()
+	{
+		List<Project> projects = new ArrayList<Project>();
 		
+		String sql = "SELECT project_id " + 
+				"        , name " + 
+				"        , from_date " + 
+				"        , to_date " + 
+				"FROM project " + 
+				"WHERE NOT(to_date IS NULL AND from_date IS NULL) " + 
+				"        AND (to_date IS NULL OR to_date > ?);";
+		
+		LocalDate today = LocalDate.now();
+		
+		SqlRowSet rows = jdbcTemplate.queryForRowSet(sql, today);
+		
+		while(rows.next())
+		{
+			Project project = new Project();
+			
+			long id = rows.getLong("project_id");
+			String name = rows.getString("name");
+			
+			LocalDate startDate = (rows.getDate("from_date") == null)
+									? null
+									: rows.getDate("from_date").toLocalDate();
+			
+			LocalDate endDate = rows.getDate("to_date") == null
+								? null
+								: rows.getDate("to_date").toLocalDate();
+			
+			project.setId(id);
+			project.setName(name);
+			project.setStartDate(startDate);
+			project.setEndDate(endDate);
+			
+			projects.add(project);
+		}
+		
+		return projects;
 	}
 
 	@Override
-	public void addEmployeeToProject(Long projectId, Long employeeId) {
+	public void removeEmployeeFromProject(Long projectId, Long employeeId)
+	{
+		String deleteQuery = "DELETE FROM project_employee WHERE projectId = ? AND employeeId = ?;";
+		
+		jdbcTemplate.update(deleteQuery, projectId, employeeId);
+	}
+
+	@Override
+	public void addEmployeeToProject(Long projectId, Long employeeId)
+	{
 		
 	}
 
