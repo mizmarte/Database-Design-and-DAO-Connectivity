f9dd97764ffe7e72c2efede937cacdd2bf00c5e2
Marte Shepherd <blootaz@rocketmail.com>
2021-01-21 19:29:37 -0500

    started setting up jdbc employee dao int testing

--- module-2/06_Database_Connectivity_DAO/pair-exercise/java/src/test/java/com/techelevator/projects/view/JDBCEmployeeDAOIntegrationTest.java
+++ module-2/06_Database_Connectivity_DAO/pair-exercise/java/src/test/java/com/techelevator/projects/view/JDBCEmployeeDAOIntegrationTest.java
@@ -3,18 +3,26 @@ package com.techelevator.projects.view;
 import static org.junit.Assert.*;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
+import com.techelevator.projects.model.jdbc.JDBCDepartmentDAO;
 import com.techelevator.projects.model.jdbc.JDBCEmployeeDAO;
+import com.techelevator.projects.model.Employee;
+import com.techelevator.projects.model.interfaces.DepartmentDAO;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class JDBCEmployeeDAOIntegrationTest
 {
 	private static SingleConnectionDataSource dataSource;
@@ -53,9 +61,33 @@ public class JDBCEmployeeDAOIntegrationTest
 	}
 
 	@Test
-	public void test()
+	public void change_employee_dept_should_return_employee_id_with_a_different_dept_id()
 	{
+		//arrange
+		
+		
+		//act
+		//assert
+		
 		fail("Not yet implemented");
+	}
+	
+	
+	
+
+	//DTO helpers
+	private Employee makeEmployee(Long departmentId, String firstName, String lastName, LocalDate birthDay, char gender, LocalDate hireDate)
+	{
+		Employee theEmployee = new Employee();
+		theEmployee.setDepartmentId(departmentId);
+		theEmployee.setFirstName(firstName);
+		theEmployee.setLastName(lastName);
+		theEmployee.setBirthDay(birthDay);
+		theEmployee.setGender(gender);
+		theEmployee.setHireDate(hireDate);
+		
+		return theEmployee;
+		
 	}
 
 }
